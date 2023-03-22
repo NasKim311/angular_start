2e9655c4c5c9fba809779abcfbb4766f3562530e
kimnamsu <kimna0311@naver.com>
2023-03-22 17:11:49 +0900

    12강 완료

--- secondStudy/src/app/app.module.ts
+++ secondStudy/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms'; // 반응형 폼 사용모듈
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, DashboardComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- secondStudy/src/app/login/login.component.ts
+++ secondStudy/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,27 +8,35 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  // ---선언부 시작
   @Input() visible1: boolean = false; // 받는 역할
   @Output() sendMyEvent: EventEmitter<boolean> = new EventEmitter(); // 보내는 역할
-  id: string = '';
-  pwd: string = '';
+
+  id: FormControl = new FormControl('');
+  pwd: FormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(4),
+  ]);
+
   private message: string = '';
+
   styleArray = { wrong_id: false, wrong_pwd: false };
+  // ---선언부 끝
 
   constructor() {}
 
   ngOnInit(): void {}
 
   tryToLogin(): void {
-    if (this.id == 'admin' && this.pwd == '1234') {
+    if (this.id.value == 'admin' && this.pwd.value == '1234') {
       alert('로그인합니다!');
       this.visible1 = true;
       this.sendMyEvent.emit(this.visible1); // app 컴포넌트에 전달
-    } else if (this.id != 'admin') {
+    } else if (this.id.value != 'admin') {
       this.setMessage = 'wrong id';
       this.styleArray.wrong_id = true;
       this.styleArray.wrong_pwd = false;
-    } else if (this.pwd != '1234') {
+    } else if (this.pwd.value != '1234') {
       this.setMessage = 'wrong pwd';
       this.styleArray.wrong_id = false;
       this.styleArray.wrong_pwd = true;
