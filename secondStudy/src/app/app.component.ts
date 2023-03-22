8fce21ab55d6b1edd90ec3aee2dfbdfe755fb18d
kimnamsu <kimna0311@naver.com>
2023-03-22 16:18:31 +0900

    11강 완료

--- secondStudy/src/app/app.component.ts
+++ secondStudy/src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   boardBool = false;
 
   getEventThanks(event: boolean) {
-    alert(event);
+    console.log(event);
     if (event == true) {
       this.loginBool = false;
       this.boardBool = true;
--- secondStudy/src/app/login/login.component.ts
+++ secondStudy/src/app/login/login.component.ts
@@ -6,21 +6,40 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  @Input() visible1: boolean = false;
-  @Output() sendMyEvent: EventEmitter<boolean> = new EventEmitter();
+  @Input() visible1: boolean = false; // 받는 역할
+  @Output() sendMyEvent: EventEmitter<boolean> = new EventEmitter(); // 보내는 역할
   id: string = '';
   pwd: string = '';
+  private message: string = '';
+  styleArray = { wrong_id: false, wrong_pwd: false };
+
   constructor() {}
 
   ngOnInit(): void {}
 
   tryToLogin(): void {
     if (this.id == 'admin' && this.pwd == '1234') {
+      alert('로그인합니다!');
       this.visible1 = true;
-    } else {
-      this.visible1 = false;
+      this.sendMyEvent.emit(this.visible1); // app 컴포넌트에 전달
+    } else if (this.id != 'admin') {
+      this.setMessage = 'wrong id';
+      this.styleArray.wrong_id = true;
+      this.styleArray.wrong_pwd = false;
+    } else if (this.pwd != '1234') {
+      this.setMessage = 'wrong pwd';
+      this.styleArray.wrong_id = false;
+      this.styleArray.wrong_pwd = true;
     }
-    console.log(this.id, this.pwd);
-    this.sendMyEvent.emit(this.visible1);
+  }
+
+  // 대입하는 메소드
+  set setMessage(arg: string) {
+    this.message = arg;
+  }
+
+  // 가져오는 메소드
+  get getMessage(): any {
+    return this.message;
   }
 }
